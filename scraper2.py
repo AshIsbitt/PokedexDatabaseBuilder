0395da9e5f8a5a7dba397bef66dfe9be8e3e0026
Ash Isbitt <Ash@ashs-mbp.home>
2020-05-27 12:51:10 +0100

    most stats written - evolve written - syntax errors aplenty

--- scraper2.py
+++ scraper2.py
@@ -29,7 +29,7 @@ def getPokemonUrls():
 	pkmn_link_list = []
 
 	listOfUrls = requests.get(pokedex_list_page)
-	soup = bs(listOfUrls.text, "lxml")
+	soup = bs(listOfUrls.text, "html.parser")
 
 	for a in soup.select('table[align="center"] td a[title*="Pok"]'):
 		pkmn_link_list.append(a.attrs.get('href'))
@@ -40,10 +40,10 @@ def getPokemonUrls():
 def getPokemonData(inputUrl):
 	pkmn_data = []
 	htmldata = requests.get(wiki_home_page + inputUrl)
-	soup = bs(htmldata.txt, 'lxml')
+	soup = bs(htmldata.txt, 'html.parser')
 
 	# Pokemon ID
-	pkmn_data.append((soup.select( 'th big a[href*="mon_by_National_Pok"] span' ).get_text().replace('#', ''))
+	pkmn_data.append(soup.select( 'th big a[href*="mon_by_National_Pok"] span' ).get_text().replace('#', ''))
 
 	#Pokemon name
 	pkmn_data.append(soup.find('h1', class_='firstHeading').text[:-9])
@@ -52,25 +52,40 @@ def getPokemonData(inputUrl):
 	pkmn_types = []
 
 	for x in soup.select('a[href*="(type)"] span b'):
-		pkmn_types.append(('a[href*="(type)"] span b').get_text())
+		pkmn_types.append(('a[href*="(type)"] span b')[x].get_text())
 
 	pkmn_data.append(pkmn_types)
 
 	#Pokemon Abilities
+	pkmn_abilities = []
+
+	for x in (soup.select('td a[href*=(Ability)] span')):
+		if soup.select('td a[href*=(Ability)] span')[x].get_text() != "Cacaphony":
+			pkmn_abilities.append(soup.select('td a[href*=(Ability)] span')[x].get_text())
+
+	pkmn_data.append(pkmn_abilities)
 
 	#Base Stats
 	# Not sure how 'td table[cellspacing="0"] tr th' works, but I'm not questioning it
-	pkmn_data.append((soup.select( 'td table[cellspacing="0"] tr th' )[ 1 ].get_text()))
-	pkmn_data.append((soup.select( 'td table[cellspacing="0"] tr th' )[ 3 ].get_text()))
-	pkmn_data.append((soup.select( 'td table[cellspacing="0"] tr th' )[ 5 ].get_text()))
-	pkmn_data.append((soup.select( 'td table[cellspacing="0"] tr th' )[ 7 ].get_text()))
-	pkmn_data.append((soup.select( 'td table[cellspacing="0"] tr th' )[ 9 ].get_text()))
-	pkmn_data.append((soup.select( 'td table[cellspacing="0"] tr th' )[ 11 ].get_text()))
-	pkmn_data.append((soup.select( 'td table[cellspacing="0"] tr th' )[ 13 ].get_text()))
+	pkmn_data.append((soup.select('td table[cellspacing="0"] tr th')[1].get_text()))
+	pkmn_data.append((soup.select('td table[cellspacing="0"] tr th')[3].get_text()))
+	pkmn_data.append((soup.select('td table[cellspacing="0"] tr th')[5].get_text()))
+	pkmn_data.append((soup.select('td table[cellspacing="0"] tr th')[7].get_text()))
+	pkmn_data.append((soup.select('td table[cellspacing="0"] tr th')[9].get_text()))
+	pkmn_data.append((soup.select('td table[cellspacing="0"] tr th')[11].get_text()))
+	pkmn_data.append((soup.select('td table[cellspacing="0"] tr th')[13].get_text()))
 
 	#Weight
+	weight_stat = soup.select('table.roundy tr td.roundy b a[href*="weight"]')[ 0 ].parent.parent
+	pkmn_data.append(float(((weight_stat.select( 'table tr td' )[ 1 ].get_text())[:-4])))
 
 	#Height
+	height_stat = soup.select('table.roundy tr td.roundy b a[href*="height"]')[ 0 ].parent.parent
+	pkmn_data.append(float(((height_stat.select( 'table tr td' )[ 1 ].get_text())[:-4])))
+
+	#EvolveLine
+
+	return pkmn_data
 
 def allPokemonStats():
 	pass
@@ -78,3 +93,5 @@ def allPokemonStats():
 def writeToDB():
 	pass
 
+
+getPokemonData("/wiki/Charmander_(Pokémon)")
